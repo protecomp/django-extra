e8fb26d5360b6d128254e7202d9ec9a26dad8064
Mikko Vilpponen <vilpponen@protecomp.fi>
2014-02-26 23:10:14 +0200

    Changed project name and added a package to setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='protecomp_django_libs',
+    name='protecomp_django_extra',
     version='',
-    packages=['protecomp', 'protecomp.fabric'],
+    packages=['protecomp', 'protecomp.fabric', 'protecomp.middleware'],
     url='',
     license='FreeBSD',
     author='Mikko Vilpponen',
     author_email='vilpponen@protecomp.fi',
-    description='Protecomp libraries for Django project management'
+    description='Snippets and code for Django projects'
 )
